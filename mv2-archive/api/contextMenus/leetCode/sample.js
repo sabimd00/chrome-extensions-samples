f46c4e245b6eb9a9eba224f90ddfdffacee8c6ae
Uzzaman,Sabir <Sabir.Uzzaman@Cerner.com>
2022-04-30 23:37:03 -0400

    Update sample.js

--- mv2-archive/api/contextMenus/leetCode/sample.js
+++ mv2-archive/api/contextMenus/leetCode/sample.js
@@ -13,18 +13,42 @@ function genericOnClick(info, tab) {
 
   src = src.replaceAll("[","{");
   src = src.replaceAll("]","}");
-
-  alert(src);
+  copyTextToClipboard(src);
+  //alert(src);
 }
 
 // Create one test item for each context type.
 var contexts = ["selection"];
 for (var i = 0; i < contexts.length; i++) {
   var context = contexts[i];
-  var title = "Convert leetcode to java array";
+  var title = "Convert to JavaArray and Copy to Clipboard";
   var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                        "onclick": genericOnClick});
   console.log("'" + context + "' item:" + id);
 }
 
+function copyTextToClipboard(text) {
+  //Create a textbox field where we can insert text to. 
+  var copyFrom = document.createElement("textarea");
 
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
+}
